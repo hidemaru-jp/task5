03b25126440f309e539b222863cc5f5b62112a9e
Hideto Inoue <111981295+hidemaru-jp@users.noreply.github.com>
2022-10-02 05:12:21 +0000

    [START]応用課題7a

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,12 +1,21 @@
 const { environment } = require('@rails/webpacker')
 
 const webpack = require('webpack')
+
 environment.plugins.prepend(
   'Provide',
   new webpack.ProvidePlugin({
     $: 'jquery/src/jquery',
     jQuery: 'jquery/src/jquery',
     Popper: 'popper.js'
+  })
+)
+
+environment.plugins.prepend('Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery/src/jquery',
+    jQuery: 'jquery/src/jquery',
+    jquery: 'jquery/src/jquery',
   })
 )
 
